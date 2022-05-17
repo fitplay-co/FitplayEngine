a5966023620297537c9203eb26f17108ca8e2f98
zhangchen <zhch0633@live.cn>
2022-05-17 19:05:47 +0800

    remove tfjs lading to

--- app/public/monitor/pose_estimation_3d/webgl_main.js
+++ app/public/monitor/pose_estimation_3d/webgl_main.js
@@ -711,9 +711,9 @@ async function startWebGL()
         str = "TF.js0  : " + time_invoke0.toFixed(1)  + " [ms]";
         dbgstr.draw_dbgstr (gl, str, 10, 10 + 22 * 1);
 
-        str = "BACKEND: " + tf.getBackend();
-        dbgstr.draw_dbgstr_ex (gl, str, win_w - 220, win_h - 22 * 3, 
-            1, [0.0, 1.0, 1.0, 1.0], [0.2, 0.2, 0.2, 1.0]);
+        // str = "BACKEND: " + tf.getBackend();
+        // dbgstr.draw_dbgstr_ex (gl, str, win_w - 220, win_h - 22 * 3, 
+            // 1, [0.0, 1.0, 1.0, 1.0], [0.2, 0.2, 0.2, 1.0]);
 
         str = "window(" + win_w + ", " + win_h + ")";
         dbgstr.draw_dbgstr (gl, str, win_w - 220, win_h - 22 * 2);
