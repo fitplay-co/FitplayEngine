564f1810e89d2d62e868372eacba7933909e1ea4
zhangchen <zhch0633@live.cn>
2022-09-14 17:37:21 +0800

    add bazel sample to integration

---
+++ deploy/integration/hello-time.cc
@@ -0,0 +1,12 @@
+#include "hello-time.h"
+#include <ctime>
+#include <iostream>
+
+void print_localtime() {
+  std::time_t result = std::time(nullptr);
+  std::cout << std::asctime(std::localtime(&result));
+}
+
+int callFunction() {
+  return 15;
+}
