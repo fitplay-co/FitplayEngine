401970d53fc970aa4072dd203d219420cbd93306
ember <detao.li@fitplay.co>
2022-09-15 18:09:13 +0800

    update mock framework

--- modules/include/actionDetection/actionManager/mockManager.hpp
+++ modules/include/actionDetection/actionManager/mockManager.hpp
@@ -11,7 +11,11 @@ namespace actionDetection {
     class mockManager {
         private:
 
-            int walkConfidence;// 0 是 不遮挡 ？ 数据正常
+            int mock = 0;
+            bool mockInit = false;
+            int mockLeftCount = 0;
+            int mockRightCount = 0;
+            int walkConfidence = 0;// 0 是 不遮挡 ？ 数据正常
             // -xyf start
             vector<vector<float> *> walkCycleLeft;
             vector<vector<float> *> tempWalkCycleLeft;
@@ -25,7 +29,8 @@ namespace actionDetection {
             mockManager();
             ~mockManager();
             void process(const Input::InputMessage* data, vector<float>&);
-            void recordWalkCycle(vector<float>*, int, int);
+            void recordWalkCycle(vector<float>, int, int);
+            void mockWalkCycle(vector<float>&);
             void calculateWalkConfidence(const PoseData::Pose* data);
     };
 
@@ -33,9 +38,43 @@ namespace actionDetection {
     mockManager::~mockManager() {}
     
     void mockManager::process(const Input::InputMessage* data, vector<float>& walkOffset) {
-        // int currentLeftStatus = walkOffset.at(0);
-        // int currentRightStatus = walkOffset.at(1);
-        recordWalkCycle(&walkOffset, walkOffset.at(0), walkOffset.at(1));
+        // walkOffset = { currentLeftStatus, currentRightStatus, currentLeftStepRate, currentRightStepRate,
+        //             meanData->at(currentLeftHipAngMean), meanData->at(currentRightHipAngMean), currentLeftStepLength,
+        //             currentRightStepLength, currentLeftProgress, currentRightProgress, currentTurnAng, currentStepRate,
+        //             currentStepLength, currentVelocity, currentVelocityThreshold, currentRealTimeLeftStatus, currentRealTimeRightStatus};
+        const PoseData::Pose* pose = data->pose();
+        calculateWalkConfidence(pose);
+        recordWalkCycle(walkOffset, walkOffset.at(0), walkOffset.at(1));
+        // high confidence : 0
+        // low confidence : -1
+        static int shiftCount = 0;
+        if(walkConfidence == 0 && walkOffset.at(0) == 0 && walkOffset.at(1) == 0) { 
+            shiftCount = 0;
+            mock = 0;
+            mockInit = false;
+            return ;
+        }
+        if(mock = 0) {
+            if(walkConfidence != 0 ) {
+                mock = 1;
+                mockInit = true;
+            }
+        }
+        if(mock = 1) {
+            if(walkConfidence == 0) {
+                shiftCount++;
+                if(shiftCount > 20) {
+                    mock = 0;
+                }
+                else {
+                    mockWalkCycle(walkOffset);
+                }
+            }
+            else {
+                shiftCount = 0;
+                mockWalkCycle(walkOffset);
+            }
+        }
     }
 
     void mockManager::calculateWalkConfidence(const PoseData::Pose* data) {
@@ -46,9 +85,60 @@ namespace actionDetection {
         // right foot
         if(data->keypoints()->Get(28)->score()>0.7 && data->keypoints()->Get(32)->score()>0.7) rightConfidence = 1;
         else rightConfidence = 0;
+
+        if(leftConfidence && rightConfidence) walkConfidence = 0;
+        else walkConfidence = -1;
     }
 
-    void mockManager::recordWalkCycle(vector<float> * walkData, int legLeft, int legRight ){
+    void mockManager::mockWalkCycle(vector<float>& walkOffset) {
+        // walkOffset = { currentLeftStatus, currentRightStatus, currentLeftStepRate, currentRightStepRate,
+        //             meanData->at(currentLeftHipAngMean), meanData->at(currentRightHipAngMean), currentLeftStepLength,
+        //             currentRightStepLength, currentLeftProgress, currentRightProgress, currentTurnAng, currentStepRate,
+        //             currentStepLength, currentVelocity, currentVelocityThreshold, currentRealTimeLeftStatus, currentRealTimeRightStatus};
+        int left = walkOffset.at(0);
+        int right = walkOffset.at(1);
+        if(mockInit) {
+            switch (left)
+            {
+            case 0:
+                mockLeftCount = ;
+                break;
+            
+            default:
+                break;
+            }
+
+            switch (right)
+            {
+            case 0:
+                mockRightCount = ;
+                break;
+            }
+            
+            mockInit = false;
+        }
+        
+        mockLeftCount++;
+        mockRightCount++;
+        int leftCycleSerial = mockLeftCount % walkCycleLeft.size();
+        int rightCycleSerial = mockRightCount % walkCycleRight.size();
+        walkOffset.at(0) = walkCycleLeft.at(mockLeftCount)->at(0);
+        walkOffset.at(1) = walkCycleRight.at(mockRightCount)->at(1);
+        walkOffset.at(2) = walkCycleLeft.at(mockLeftCount)->at(2);
+        walkOffset.at(3) = walkCycleRight.at(mockRightCount)->at(3);
+        walkOffset.at(4) = walkCycleLeft.at(mockLeftCount)->at(4);
+        walkOffset.at(5) = walkCycleRight.at(mockRightCount)->at(5);
+        walkOffset.at(6) = walkCycleLeft.at(mockLeftCount)->at(6);
+        walkOffset.at(7) = walkCycleRight.at(mockRightCount)->at(7);
+        walkOffset.at(8) = walkCycleLeft.at(mockLeftCount)->at(8);
+        walkOffset.at(9) = walkCycleRight.at(mockRightCount)->at(9);
+
+        walkOffset.at(11) = (walkCycleLeft.at(mockLeftCount)->at(11) + walkCycleRight.at(mockRightCount)->at(11))/2;
+        walkOffset.at(12) = (walkCycleLeft.at(mockLeftCount)->at(12) + walkCycleRight.at(mockRightCount)->at(12))/2;
+        walkOffset.at(13) = (walkCycleLeft.at(mockLeftCount)->at(13) + walkCycleRight.at(mockRightCount)->at(13))/2;
+    }
+
+    void mockManager::recordWalkCycle(vector<float> walkData, int legLeft, int legRight ){
         //左脚踝
         if(walkConfidence == 0){//说明一个正常的数据来了
             if((legLeft == 0 &&  tempWalkCycleLeft.size() && tempWalkCycleLeft.back() == -1) || (legLeft == 1 &&  tempWalkCycleLeft.size() && tempWalkCycleLeft.back() == -1) ) //说明一个循环到了 分两种情况
