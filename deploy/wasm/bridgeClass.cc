178e55bab350e3b1cfe06c2f25f006e54a1a7fab
wangchong <wangchong@wangchongdeMacBook-Pro.local>
2022-09-21 15:32:16 +0800

    提取bridgeClass公共部分放入midware

--- deploy/wasm/bridgeClass.cc
+++ deploy/wasm/bridgeClass.cc
@@ -3,17 +3,7 @@
 #include <emscripten/val.h>
 #include <string>
 #include <vector>
-#include "fitting/fitting_component.hpp"
-#include "poseData_generated.h"
-#include "actionData_generated.h"
-#include "featureConfig_generated.h"
-#include "inputMessage_generated.h"
-#include "generalDetection/generalCalculator.hpp"
-#include "actionDetection/actionDetectionComponent.hpp"
-#include "gazeTracking/gazeTracking.hpp"
-#include "groundLocation/groundLocation.hpp"
-#include "midwareComponent/midwareComponent.hpp"
-#include "midwareComponent/midwareManager.hpp"
+#include "bridgeClass.hpp"
 #include <glog/logging.h>
 #include <stdio.h>
 #include "ceres/ceres.h"
@@ -35,30 +25,20 @@ struct CostFunctor {
   }
 };
 
-class BridgeClass {
+class JSMidwareBridgeClass {
 public:
-  BridgeClass()
+  JSMidwareBridgeClass()
   {
-    midwareManager.registerMidwareComponent(&groundInstance);
-    midwareManager.registerMidwareComponent(&gazeInstance);
-    midwareManager.registerMidwareComponent(&actionDetectionInstance);
-    midwareManager.registerMidwareComponent(&fittingInstance);
-    midwareManager.registerMidwareComponent(&generalInstance);
-
     //init glog 
     google::InitGoogleLogging("FitplayEngine");
   }
 
   void release() {
-    if (action_data.GetSize() > 0) {
-      action_data.Release();
-    }
+    midwareBridge.release();
   }
 
   val entry(std::string inputData) {
   
-    const Input::InputMessage* inputMessage = Input::GetInputMessage(&inputData[0]);
-    
     LOG(INFO) << "Found " << "glog started where to go" << " cookies";
     cout << "enter wasm module careful, printing will cause performance lag!!!" << endl;
     cout << "cere hello world start" << endl;
@@ -89,43 +69,27 @@ public:
 
     cout << "cere hello world end" << endl;
 
-    vector<int> componentIndexWithOutput;
-    for (int i = 0; i < midwareManager.componentList.size(); i++) {
-      bool hasOutput = midwareManager.componentList.at(i)->handleInput(inputMessage, action_data);
-      if (hasOutput) {
-        componentIndexWithOutput.push_back(i);
-      }
+    midwareBridge.processData(&inputData[0]);
+    if (midwareBridge.hasOutput()) {
+      uint8_t *byteBuffer = midwareBridge.getCurrentBuffer();
+      size_t bufferLength = midwareBridge.getCurrentBufferSize();
+      return val(typed_memory_view(bufferLength <= 0 ? 0 : bufferLength, byteBuffer));
+    } else {
+      return val(typed_memory_view(0, &inputData[0]));
     }
-
-    actionData::ActionBuilder actionBuilder(action_data);
-
-    for (int i = 0; i < componentIndexWithOutput.size(); i++) {
-      midwareManager.componentList.at(componentIndexWithOutput.at(i))->writeToFlatbuffers(actionBuilder);
-    }
-
-    auto build = actionBuilder.Finish();
-    action_data.Finish(build);
-    uint8_t *byteBuffer = action_data.GetBufferPointer();
-    size_t bufferLength = action_data.GetSize();
-    return val(typed_memory_view(componentIndexWithOutput.size() <= 0 ? 0 : bufferLength, byteBuffer));
+    
   }
 
 
 private:
-  fitplay::fittingComponent fittingInstance;
-  generalDetection::generalCalculator generalInstance;
-  actionDetection::actionDetectionComponent actionDetectionInstance;
-  gaze::gazeTracking gazeInstance;
-  ground::groundLocation groundInstance;
-  flatbuffers::FlatBufferBuilder action_data;
-  Midware::MidwareManager midwareManager;
+  fitplayBridge::BridgeClass midwareBridge;
 };
 
 // Binding code
 EMSCRIPTEN_BINDINGS(my_class_example) {
-  class_<BridgeClass>("BridgeClass")
+  class_<JSMidwareBridgeClass>("BridgeClass")
     .constructor<>()
-    .function("entry", &BridgeClass::entry)
-    .function("release", &BridgeClass::release)
+    .function("entry", &JSMidwareBridgeClass::entry)
+    .function("release", &JSMidwareBridgeClass::release)
     ;
 }
